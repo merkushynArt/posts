004de3ba5d4539b4201685850c67b97b8f3f4f28
Artem <merkushynwrite@gmail.com>
2022-12-22 13:22:34 +0200

    added basic settimgs to express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { json } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
@@ -11,16 +12,26 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
+// Middleware - для розширення функцыоналу express
+// Для відправки запросів з різних ip-адресів
+app.use(cors());
+
+//Для того щобexpress розумів що дані с фронту будуть приходити у форматі json
+app.use(express.json());
+
+app.get('/', (req, res) => {
+   res.json({message: 'All is fine'})
+})
+
 
 async function start() {
    try {
       //для підголтовки к оновленню mongoose
       mongoose.set('strictQuery', false);
-
       await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.r9005s1.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,)
 
       app.listen(PORT, () => {
-         console.log(`server started on port: ${PORT}`);
+         console.log(`server started on http://localhost:${PORT}/`);
       });
    } catch(error) {
       console.log(error);
