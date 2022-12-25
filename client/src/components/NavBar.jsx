a9d6f752383b70836c177940e9e4f238102d65ee
Artem <merkushynwrite@gmail.com>
2022-12-25 21:50:36 +0200

    made a layout of register and login pages

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
-   const isAuth = true;
+   const isAuth = false;
 
    const activeStyles = {
       color: 'white',
