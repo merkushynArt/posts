7511d68e0d72b12053626b4459f0a5ee96be55c7
Artem <merkushynwrite@gmail.com>
2022-12-27 12:22:47 +0200

    made a postSlice and connected in store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import  authSlice from './features/auth/authSlice.js';
+import authSlice from './features/auth/authSlice.js';
+import postSlice from './features/post/postSlice.js';
 
 export const store = configureStore({
    reducer: {
       auth: authSlice,
+      post: postSlice,
    },
 });
