2303b3109180c855dd4f53e41e6caa1ab46e021f
Artem <merkushynwrite@gmail.com>
2022-12-30 11:34:55 +0200

    made layout on the main-page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 export const MainPage = () => {
    return (
-      <div>MainPage</div>
+      <div className='max-w-[900px] mx-auto py-10'>
+         <div className='flex justify-between gap-8'>
+            <div className='flex flex-col gap-10 basis-4/5'>
+               Posts
+            </div>
+            <div className='basis-1/5'>
+               <div className='text-xs uppercase text-white'>
+                  Популярні пости
+               </div>
+            </div>
+         </div>
+      </div>
    )
 }
