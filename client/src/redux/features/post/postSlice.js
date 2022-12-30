cd7381a08a4cf4755ee0ad58174135a1be6a4f57
Artem <merkushynwrite@gmail.com>
2022-12-30 11:26:39 +0200

    made a slice to get posts

--- client/src/redux/features/post/postSlice.js
+++ client/src/redux/features/post/postSlice.js
@@ -17,6 +17,18 @@ export const createPost = createAsyncThunk(
          console.log(error);
       }
    }
+);
+
+export const getAllPosts = createAsyncThunk(
+   'post/getAllPosts',
+   async () => {
+      try {
+         const { data } = await axios.get('/posts');
+         return data;
+      } catch (error) {
+         console.log(error);
+      }
+   }
 )
 
 export const postSlice = createSlice({
@@ -33,6 +45,18 @@ export const postSlice = createSlice({
          state.posts.push(action.payload);
       },
       [createPost.rejected]: (state) => {
+         state.loading = false;
+      },
+      // Get all posts
+      [getAllPosts.pending]: (state) => {
+         state.loading = true;
+      },
+      [getAllPosts.fulfilled]: (state, action) => {
+         state.loading = false;
+         state.posts = action.payload.posts;
+         state.popularPosts = action.payload.popularPosts;
+      },
+      [getAllPosts.rejected]: (state) => {
          state.loading = false;
       },
    },
