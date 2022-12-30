81a7087e98cadb38a265a9a0a50bcd1823b5d938
Artem <merkushynwrite@gmail.com>
2022-12-30 21:07:06 +0200

    made layout PostPage and worked with props

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { AiFillEye, AiOutlineMessage  } from "react-icons/ai";
+import Moment from 'react-moment';
+import axios from '../utils/axios.js'
 
 export const PostPage = () => {
+   const [post, setPost] = useState(null);
+
+   const params = useParams();
+
+   const fetchPost = useCallback(async () => {
+      const { data } = await axios.get(`/posts/${params.id}`);
+      setPost(data);
+   }, [params.id]);
+
+   useEffect(() => {
+      fetchPost();
+   }, [fetchPost]);
+
+   if (!post) {
+      return (
+         <div className='text-xl text-center text-white py-10'>
+            Загрузка...
+         </div>
+      )
+   }
+
    return (
-      <div>PostPage</div>
+      <div>
+
+         <button className='flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4'>
+            <Link className='flex' to={'/'}>
+               Назад
+            </Link>
+         </button>
+
+         <div className='flex gap-10 py-8'>
+
+            <div className='w-2/3'>
+               <div className='flex flex-col basis-1/4 flex-grow'>
+                  <div
+                     className={
+                        post?.imgUrl
+                           ? 'flex rouded-sm h-80'
+                           : 'flex rounded-sm'
+                     }
+                  >
+                     {post?.imgUrl && (
+                        <img
+                           src={`http://localhost:3002/${post.imgUrl}`}
+                           alt='img'
+                           className='object-cover w-full'
+                        />
+                     )}
+                  </div>
+               </div>
+
+               <div className='flex justify-between items-center pt-2'>
+                  <div className='text-xs text-white opacity-50'>
+                     {post.username}
+                  </div>
+                  <div className='text-xs text-white opacity-50'>
+                     <Moment date={post.createdAt} format='D MMM YYYY' />
+                  </div>
+               </div>
+               
+               <div className='text-white text-xl'>{post.title}</div>
+
+               <p className='text-white opacity-60 text-xs pt-4'>
+                  {post.text}
+               </p>
+            </div>
+
+            <div className='w-1/3 p-8 bg-gray-700 flex flex-col gap-2 rounded-sm'>Популярне:</div>
+
+         </div>
+
+      </div>
    )
 }
