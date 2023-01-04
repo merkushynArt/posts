1489e96bd87099847dc8bc64d67451684d9192c9
Artem <merkushynwrite@gmail.com>
2023-01-04 10:10:34 +0200

    implemented a editing posts on client-side

--- client/src/redux/features/post/postSlice.js
+++ client/src/redux/features/post/postSlice.js
@@ -40,6 +40,21 @@ export const removePost = createAsyncThunk('post/removePost', async (id) => {
    }
 });
 
+export const updatePost = createAsyncThunk(
+   'post/updatePost',
+   async (updatedPost) => {
+      try {
+         const { data } = await axios.put(
+               `/posts/${updatedPost.id}`,
+               updatedPost,
+         )
+         return data;
+      } catch (error) {
+         console.log(error);
+      }
+   },
+);
+
 export const postSlice = createSlice({
    name: 'post',
    initialState,
@@ -81,7 +96,21 @@ export const postSlice = createSlice({
       },
       [removePost.rejected]: (state) => {
          state.loading = false;
-      },  
+      },
+      // Оновлення посту
+      [updatePost.pending]: (state) => {
+         state.loading = true;
+      },
+      [updatePost.fulfilled]: (state, action) => {
+         state.loading = false;
+         const index = state.posts.findIndex(
+            (post) => post._id === action.payload._id
+         )
+         state.posts[index] = action.payload;
+      },
+      [updatePost.rejected]: (state) => {
+         state.loading = false;
+      },
    },
 });
 
