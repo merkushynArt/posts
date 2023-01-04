1fdf057a7a2750d526ad4c4e00ec5fbadcf69416
Artem <merkushynwrite@gmail.com>
2023-01-04 12:06:37 +0200

    made links to componets of PopularPosts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,7 +4,7 @@ import { checkAuth } from "../utils/checkAuth.js";
 
 const router = new Router();
 
-// Create Post http://localhost:3002/api/posts
+// Create post http://localhost:3002/api/posts
 router.post('/', checkAuth, createPost);
 
 // Get all posts http://localhost:3002/api/posts,
