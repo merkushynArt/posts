6d29f061ca33d29768322b019c07d66a1f7dca86
Artem <merkushynwrite@gmail.com>
2023-02-07 20:30:59 +0200

    fix image path

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -21,7 +21,7 @@ export const PostItem = ({ post }) => {
                {post.imgUrl && (
                   <img
                      //http://localhost:3002/
-                     src={`https://posts-api-three.vercel.app/${post.imgUrl}`}
+                     src={`http://localhost:3002/${post.imgUrl}`}
                      alt='img'
                      className='object-cover w-full'
                   />
